29597e931f2c7fbc893275fd6ff8598181c9a35b
BILD\jean.garcia <jean.garcia@bild.com.br>
2021-12-15 11:52:59 -0300

    url of customer

--- config/iss-crm.php
+++ config/iss-crm.php
@@ -3,6 +3,13 @@
 return [
 
     'base_uri' => env('MS_CRM_BASE_URI', 'https://api.almobi.com.br'),
+    'prefix' => env('MS_CRM_API_PREFIX', '/api'),
 
-    'prefix' => env('MS_CRM_API_PREFIX', '/api')
+    'front_uri' => env('MS_HUB_FRONT_URI', 'https://develop.crm.nave.dev'),
+
+    'redirects' => [
+        'customers' => [
+            'show' => '/customers/%s'
+        ]
+    ]
 ];
