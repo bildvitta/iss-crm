21c7af79e00e1d594a82237b8683a0fcd33ca3b8
noemimpimentel <noemimpimentel@gmail.com>
2025-01-13 17:44:21 -0300

    no_unused_imports

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -4,8 +4,7 @@ namespace Bildvitta\IssCrm\Models;
 
 use Bildvitta\IssCrm\Traits\UsesCrmDB;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Ramsey\Uuid\Uuid;
+
 
 class Country extends Model
 {
