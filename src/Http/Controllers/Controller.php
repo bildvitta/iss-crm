c6904679157f4f8caa5ad85e26277e1e4b96c947
FilipeNSV <filipe.nsv@gmail.com>
2025-09-24 18:18:40 -0300

    Atualização de um cliente pelo iss-crm

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bildvitta\IssCrm\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
+
+    /**
+     * @var JsonResponse
+     */
+    protected JsonResponse $jsonResponse;
+
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+        $this->jsonResponse = app(JsonResponse::class);
+    }
+}
---
+++ src/Http/Controllers/Customers/CustomerController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Bildvitta\IssCrm\Http\Controllers\Documents;
+
+use Bildvitta\IssCrm\Http\Controllers\Controller;
+
+class CustomerController extends Controller
+{}
